1db6ca9eba5530c589074f9771dfe755f82b8802
nikang <mohamadnikan2013@gmail.com>
2017-01-12 00:08:58 +0330

    Ui completed only UI

--- table.py
+++ table.py
@@ -3,16 +3,35 @@ import turtle
 
 class File:
     def __init__(self, fileName):
-        self.fileName = fileName
+        self._fileName = fileName
+        self.data = [line.split() for line in open(fileName)]
+        self.data.pop(-1)
+        self.up = self.data[0][1:6]
+        self.down = self.data[6][1:6]
+        self.left = self.column(1)[1:6]
+        self.right = self.column(6)[1:6]
+        self.table = self.gettable()
+        print("hello world")
+
+    def column(self, i):
+        return [row[i] for row in self.data[0:7]]
+
+    def gettable(self):
+        return [row[1:6] for row in self.data[1:6]]
 
 
 class BoardUI:
-    def __init__(self):
+    def __init__(self, file):
         self.x_coord = -250
         self.y_coord = 300
         self.side = 80
+        self.file = file
+        self.draw_board()
+        self.draw_down_condition()
+        self.draw_up_condition()
+        self.draw_left_condition()
+        self.draw_right_condition()
 
-    # TODO read file and complete action for UI @mohamadnikan2013@gmail.com
     def draw_board(self):
         myxcoord = self.x_coord
         myycoord = self.y_coord
@@ -24,9 +43,9 @@ class BoardUI:
 
         for i in range(dimension ** 2):
             if i % dimension == 0:
-                self.y_coord -= self.side
+                myycoord -= self.side
                 my_pen.penup()
-                my_pen.setpos(self.x_coord, self.y_coord)
+                my_pen.setpos(myxcoord, myycoord)
                 my_pen.pendown()
 
             for _ in range(4):
@@ -40,7 +59,7 @@ class BoardUI:
         lalist = []
         for i in range(1, 6):
             for j in range(5):
-                lalist.append([myxcoord + (j * self.side), myycoord - (self.side * i)])
+                lalist.append([self.x_coord + (j * self.side), self.y_coord - (self.side * i)])
         my_pen.penup()
         index = 0
         for i in lalist:
@@ -48,29 +67,31 @@ class BoardUI:
             my_pen.write(dic[index])
             index += 1
 
-    def draw_up_condition(self, x):
+    def draw_up_condition(self):
         my_pen = turtle.Turtle()
         my_pen.speed('fastest')
         my_pen.hideturtle()
         my_pen.pensize(3)
         my_pen.penup()
         my_pen.goto(self.x_coord + 40, self.y_coord - 70)
-        for i in x:
-            my_pen.write(i)
+        for i in self.file.up:
+            if i != '0':
+                my_pen.write(i)
             my_pen.forward(self.side)
 
-    def draw_down_condition(self, x):
+    def draw_down_condition(self):
         my_pen = turtle.Turtle()
         my_pen.speed('fastest')
         my_pen.hideturtle()
         my_pen.pensize(3)
         my_pen.penup()
         my_pen.goto(self.x_coord + 40, self.y_coord - 500)
-        for i in x:
-            my_pen.write(i)
+        for i in self.file.down:
+            if i != '0':
+                my_pen.write(i)
             my_pen.forward(self.side)
 
-    def draw_left_condition(self, x):
+    def draw_left_condition(self):
         my_pen = turtle.Turtle()
         my_pen.speed('fastest')
         my_pen.hideturtle()
@@ -78,11 +99,12 @@ class BoardUI:
         my_pen.penup()
         my_pen.goto(self.x_coord - 15, self.y_coord - 120)
         my_pen.right(90)
-        for i in x:
-            my_pen.write(i)
+        for i in self.file.left:
+            if i != '0':
+                my_pen.write(i)
             my_pen.forward(self.side)
 
-    def draw_right_condition(self, x):
+    def draw_right_condition(self):
         my_pen = turtle.Turtle()
         my_pen.speed('fastest')
         my_pen.hideturtle()
@@ -90,8 +112,9 @@ class BoardUI:
         my_pen.penup()
         my_pen.goto(self.x_coord + 420, self.y_coord - 120)
         my_pen.right(90)
-        for i in x:
-            my_pen.write(i)
+        for i in self.file.right:
+            if i != '0':
+                my_pen.write(i)
             my_pen.forward(self.side)
 
 
@@ -102,6 +125,8 @@ class Grid:
         self.y = y
         self.x = x
         self.name = name
+        if self.is_initialized:
+            self.write(self.val)
 
     def write(self, new_val):
         if self.is_initialized:
@@ -129,7 +154,7 @@ class Grid:
 
 
 class BoardLogic:
-    def __init__(self):
+    def __init__(self, file):
         self.x_coord = -250
         self.y_coord = 300
         self.side = 80
@@ -156,4 +181,7 @@ class BoardLogic:
 
 
 if __name__ == '__main__':
+    ff = File("static/e1.txt")
+    Ui = BoardUI(ff)
+    logic = BoardLogic(ff)
     turtle.exitonclick()
