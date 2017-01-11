873ba90dcea0bc412413728e26a4c90850cd02e5
nikang <mohamadnikan2013@gmail.com>
2017-01-11 23:08:15 +0330

    new class define :D  ;D

--- table.py
+++ table.py
@@ -1,8 +1,98 @@
 import turtle
 
 
-class Board:
-    pass
+class File:
+    def __init__(self, fileName):
+        self.fileName = fileName
+
+
+class BoardUI:
+    def __init__(self):
+        self.x_coord = -250
+        self.y_coord = 300
+        self.side = 80
+
+    # TODO read file and complete action for UI @mohamadnikan2013@gmail.com
+    def draw_board(self):
+        myxcoord = self.x_coord
+        myycoord = self.y_coord
+        my_pen = turtle.Turtle()
+        my_pen.speed('fastest')
+        my_pen.hideturtle()
+        my_pen.pensize(3)
+        dimension = 5
+
+        for i in range(dimension ** 2):
+            if i % dimension == 0:
+                self.y_coord -= self.side
+                my_pen.penup()
+                my_pen.setpos(self.x_coord, self.y_coord)
+                my_pen.pendown()
+
+            for _ in range(4):
+                my_pen.forward(self.side)
+                my_pen.right(90)
+
+            my_pen.forward(self.side)
+        dic = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z']
+        lalist = []
+        for i in range(1, 6):
+            for j in range(5):
+                lalist.append([myxcoord + (j * self.side), myycoord - (self.side * i)])
+        my_pen.penup()
+        index = 0
+        for i in lalist:
+            my_pen.goto(i[0] + 4, i[1] - 19)
+            my_pen.write(dic[index])
+            index += 1
+
+    def draw_up_condition(self, x):
+        my_pen = turtle.Turtle()
+        my_pen.speed('fastest')
+        my_pen.hideturtle()
+        my_pen.pensize(3)
+        my_pen.penup()
+        my_pen.goto(self.x_coord + 40, self.y_coord - 70)
+        for i in x:
+            my_pen.write(i)
+            my_pen.forward(self.side)
+
+    def draw_down_condition(self, x):
+        my_pen = turtle.Turtle()
+        my_pen.speed('fastest')
+        my_pen.hideturtle()
+        my_pen.pensize(3)
+        my_pen.penup()
+        my_pen.goto(self.x_coord + 40, self.y_coord - 500)
+        for i in x:
+            my_pen.write(i)
+            my_pen.forward(self.side)
+
+    def draw_left_condition(self, x):
+        my_pen = turtle.Turtle()
+        my_pen.speed('fastest')
+        my_pen.hideturtle()
+        my_pen.pensize(3)
+        my_pen.penup()
+        my_pen.goto(self.x_coord - 15, self.y_coord - 120)
+        my_pen.right(90)
+        for i in x:
+            my_pen.write(i)
+            my_pen.forward(self.side)
+
+    def draw_right_condition(self, x):
+        my_pen = turtle.Turtle()
+        my_pen.speed('fastest')
+        my_pen.hideturtle()
+        my_pen.pensize(3)
+        my_pen.penup()
+        my_pen.goto(self.x_coord + 420, self.y_coord - 120)
+        my_pen.right(90)
+        for i in x:
+            my_pen.write(i)
+            my_pen.forward(self.side)
 
 
 class Grid:
@@ -38,118 +128,32 @@ class Grid:
         my_pen.write(self.val)
 
 
-def draw_board(x_coord=-250, y_coord=300, side=80):
-    myxcoord = x_coord
-    myycoord = y_coord
-    my_pen = turtle.Turtle()
-    my_pen.speed('fastest')
-    my_pen.hideturtle()
-    my_pen.pensize(3)
-    dimension = 5
+class BoardLogic:
+    def __init__(self):
+        self.x_coord = -250
+        self.y_coord = 300
+        self.side = 80
+        dic = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z']
+        lalist = []
+        for i in range(1, 6):
+            for j in range(5):
+                lalist.append([self.x_coord + (j * self.side), self.y_coord - (self.side * i)])
 
-    for i in range(dimension ** 2):
-        if i % dimension == 0:
-            y_coord -= side
-            my_pen.penup()
-            my_pen.setpos(x_coord, y_coord)
-            my_pen.pendown()
-
-        for _ in range(4):
-            my_pen.forward(side)
-            my_pen.right(90)
-
-        my_pen.forward(side)
-    dic = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-           'w', 'x', 'y', 'z']
-    lalist = []
-    for i in range(1, 6):
-        for j in range(5):
-            lalist.append([myxcoord + (j * side), myycoord - (side * i)])
-    my_pen.penup()
-    index = 0
-    for i in lalist:
-        my_pen.goto(i[0] + 4, i[1] - 19)
-        my_pen.write(dic[index])
+        index = 0
+        self.grids = []
+        for i in lalist:
+            g = Grid(x=i[0], y=i[1], name=dic[index])
+        self.grids.append(g)
         index += 1
 
+    def solve(self):
+        pass
 
-def draw_up_condition(x, x_coord=-250, y_coord=300, side=80):
-    my_pen = turtle.Turtle()
-    my_pen.speed('fastest')
-    my_pen.hideturtle()
-    my_pen.pensize(3)
-    my_pen.penup()
-    my_pen.goto(x_coord + 40, y_coord - 70)
-    for i in x:
-        my_pen.write(i)
-        my_pen.forward(side)
-
-
-def draw_down_condition(x, x_coord=-250, y_coord=300, side=80):
-    my_pen = turtle.Turtle()
-    my_pen.speed('fastest')
-    my_pen.hideturtle()
-    my_pen.pensize(3)
-    my_pen.penup()
-    my_pen.goto(x_coord + 40, y_coord - 500)
-    for i in x:
-        my_pen.write(i)
-        my_pen.forward(side)
-
-
-def draw_left_condition(x, x_coord=-250, y_coord=300, side=80):
-    my_pen = turtle.Turtle()
-    my_pen.speed('fastest')
-    my_pen.hideturtle()
-    my_pen.pensize(3)
-    my_pen.penup()
-    my_pen.goto(x_coord - 15, y_coord - 120)
-    my_pen.right(90)
-    for i in x:
-        my_pen.write(i)
-        my_pen.forward(side)
-
-
-def draw_right_condition(x, x_coord=-250, y_coord=300, side=80):
-    my_pen = turtle.Turtle()
-    my_pen.speed('fastest')
-    my_pen.hideturtle()
-    my_pen.pensize(3)
-    my_pen.penup()
-    my_pen.goto(x_coord + 420, y_coord - 120)
-    my_pen.right(90)
-    for i in x:
-        my_pen.write(i)
-        my_pen.forward(side)
-
-
-def initial_board(x_coord=-250, y_coord=300, side=80):
-    dic = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-           'w', 'x', 'y', 'z']
-    lalist = []
-    for i in range(1, 6):
-        for j in range(5):
-            lalist.append([x_coord + (j * side), y_coord - (side * i)])
-
-    index = 0
-    grids = []
-    for i in lalist:
-        g = Grid(x=i[0], y=i[1], name=dic[index])
-        grids.append(g)
-        index += 1
-    return grids
-
-
-def solve():
-    pass
-
-
-def check():
-    pass
+    def check(self):
+        pass
 
 
 if __name__ == '__main__':
-    draw_board()
-    TT = initial_board()
-
     turtle.exitonclick()
